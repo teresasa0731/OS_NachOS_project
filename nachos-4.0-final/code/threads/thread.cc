72a94185a38f1a50c945f37580dd44b3af4a5360
teresasa0731 <85164593+teresasa0731@users.noreply.github.com>
2024-06-20 17:45:45 +0800

    fix typo in thread

--- nachos-4.0-final/code/threads/thread.cc
+++ nachos-4.0-final/code/threads/thread.cc
@@ -278,9 +278,7 @@ Thread::Sleep (bool finishing)
     // 2. Reset some value of current_thread, then context switch
     this->setRemainingBurstTime(this->getRemainingBurstTime() - this->getRRTime());
     this->setRunTime(this->getRunTime() + this->getRRTime());
-    if(!finishing){
-        DEBUG('z',"[UpdateRemainingBurstTime] Tick [" << kernel->stats->totalTicks << "]: Thread [" << this->getID()  << "] update approximate burst time, from: [" << this->getRemainingBurstTime() + this->getRRTime() << "] - [" << this->getRRTime() << "], to [" << this->getRemainingBurstTime() << "]");
-    }
+    DEBUG('z',"[UpdateRemainingBurstTime] Tick [" << kernel->stats->totalTicks << "]: Thread [" << this->getID()  << "] update approximate burst time, from: [" << this->getRemainingBurstTime() + this->getRRTime() << "] - [" << this->getRRTime() << "], to [" << this->getRemainingBurstTime() << "]");
     this->setRRTime(0);
     if (nextThread != NULL && this != nextThread) {
         DEBUG('z',"[ContextSwitch] Tick [" << kernel->stats->totalTicks << "]: Thread [" << nextThread->getID()  << "] is now selected for execution, thread [" << this->getID() << "] is replaced, and it has executed [" << this->getRunTime() << "] ticks");
